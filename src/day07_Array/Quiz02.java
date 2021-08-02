e241eef2a46fb3abd976b648a84942f57a0279fd
jinsung <tjd610@naver.com>
2021-08-02 14:01:26 +0900

    ArrayList 퀴즈복습

--- src/day07_Array/Quiz02.java
+++ src/day07_Array/Quiz02.java
@@ -7,11 +7,10 @@ public class Quiz02 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		String n,p;
+	/*	String n,p;
 		int n1=0;
 		ArrayList name = new ArrayList();
 		ArrayList phone = new ArrayList();
-		
 		while(true) {
 			System.out.println("1.연락처 등록");
 			System.out.println("2.연락처 보기");
@@ -21,7 +20,7 @@ public class Quiz02 {
 			System.out.print(">>> ");
 			n1 = input.nextInt();
 			if(n1 == 5) {
-				System.out.println("프로그램 종료");
+				System.out.println("프로그램 종료 ~ !");
 				break;
 			}
 			switch(n1) {
@@ -61,7 +60,70 @@ public class Quiz02 {
 				break;
 			}
 			System.out.println("");
+		}*/
+		
+		String n,p;
+		int n1=0;
+		ArrayList name = new ArrayList();
+		ArrayList phone = new ArrayList();
+		
+		while(true) {
+		System.out.println("1. 연락처 등록");
+		System.out.println("2. 연락처 보기");
+		System.out.println("3. 연락처 삭제");
+		System.out.println("4. 모든 연락처 보기");
+		System.out.println("5. 종료");
+		System.out.println(">>> ");
+		n1=input.nextInt();
+		if(n1 == 5) {
+			System.out.println("시스템 종료합니다 ~");
+			break;
 		}
+		switch(n1) {
+			case 1:
+				System.out.println("이름 입력 : ");
+				n = input.next();
+				if(name.indexOf(n) == -1) {
+					System.out.println("연락처 입력 : ");
+					p = input.next();
+					name.add(n);
+					phone.add(p);
+				}else {
+					System.out.println("이름이 존재 합니다.");
+			}break;
+			case 2:
+				System.out.println("찾을 이름 입력 : ");
+				n = input.next();
+				if(name.indexOf(n)== -1) {
+					System.out.println(n+"님은 연락처가 없습니다.");
+				}else {
+					System.out.println(name.get(name.indexOf(n))+" : "+phone.get(name.indexOf(n)));
+				}break;
+			case 3:
+				System.out.println("삭제할 이름 : ");
+				n=input.next();
+				if(name.indexOf(n)== -1) {
+					System.out.println("삭제할 이름이 존재하지 않습니다.");
+				}else {
+					System.out.print(name.remove(name.indexOf(n))+" : ");
+					System.out.println(phone.remove(name.indexOf(n))+"삭제");
+				}break;
+			case 4:
+				for(int i = 0; i<name.size();i++) {
+					System.out.println(name.get(i)+ " : "+phone.get(i));
+				}
+		}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
